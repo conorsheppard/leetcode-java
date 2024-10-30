d134ab0dfc8c2e71ff77ce9e63fe6d898aa69582
Conor Sheppard <conorsheppardwork@gmail.com>
2024-10-30 23:00:14 +0000

    added MergeSortedArray and tests

---
+++ src/main/java/org/example/MergeSortedArray.java
@@ -0,0 +1,73 @@
+package org.example;
+
+public class MergeSortedArray {
+
+    public static int[] merge(int[] nums1, int m, int[] nums2, int n) {
+        if (n == 0) {
+            return nums1;
+        }
+        if (m == 0) {
+            return nums2;
+        }
+        int[] newNums = new int[m + n];
+        int index = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < (m + n); j++) {
+                // if j is greater or equal to m, add the rest of the contents to newNums[]
+                if (j >= m) {
+                    for (int k = m + 1; k < (m + n); k++) {
+                        newNums[k] = nums2[i];
+                        i++;
+                        j++;
+                    }
+                } else if (nums2[i] < nums1[j]) {
+                    newNums[index] = nums2[i]; // index++
+                    i++;
+                    index++;
+                } else if (nums1[j] < nums2[i]) {
+                    newNums[index] = nums1[j]; // index++
+                    index++;
+                } else if (nums1[j] == nums2[i]) {
+                    newNums[index] = nums1[j]; // index++
+                    index++;
+                    newNums[index] = nums2[i]; // index++
+                    index++;
+                    i++;
+                }
+            }
+        }
+        nums1 = newNums;
+//        for (int y = 0; y < nums1.length; y++) nums1[y] = newNums[y];
+        return nums1;
+    }
+
+    public void mergeLeetCodeVersion(int[] nums1, int m, int[] nums2, int n) {
+        if (n == 0) {
+            return;
+        }
+        if (m == 0) {
+            nums1[0] = nums2[0];
+            return;
+        }
+        int[] newNums = new int[m+n];
+        int index = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < (m+n); j++) {
+                if (j >= m) {
+                    for (int k = m + 1; k < (m + n); k++) {
+                        newNums[k] = nums2[i++];
+                        j++;
+                    }
+                } else if (nums2[i] < nums1[j]) {
+                    newNums[index++] = nums2[i++];
+                } else if (nums1[j] < nums2[i]) {
+                    newNums[index++] = nums1[j];
+                } else if (nums1[j] == nums2[i]) {
+                    newNums[index++] = nums1[j];
+                    newNums[index++] = nums2[i++];
+                }
+            }
+        }
+        System.arraycopy(newNums, 0, nums1, 0, nums1.length);
+    }
+}
---
+++ src/test/java/org/example/MergeSortedArrayTest.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+
+@SuppressWarnings("checkstyle:MissingJavadocType")
+public class MergeSortedArrayTest {
+
+    @Test
+    public void testCase1() {
+        var nums1 = new int[]{1, 2, 3, 0, 0, 0};
+        var nums2 = new int[]{2, 5, 6};
+        int m = 3, n = 3;
+        var expected = new int[]{1, 2, 2, 3, 5, 6};
+        var result = MergeSortedArray.merge(nums1, m, nums2, n);
+        assertEquals(Arrays.toString(expected), Arrays.toString(result));
+    }
+
+    @Test
+    public void testCase2() {
+        int[] nums1 = new int[]{1}, nums2 = new int[]{};
+        int m = 1, n = 0;
+        var expected = new int[]{1};
+        var result = MergeSortedArray.merge(nums1, m, nums2, n);
+        assertEquals(Arrays.toString(expected), Arrays.toString(result));
+    }
+
+    @Test
+    public void testCase3() {
+        int[] nums1 = new int[]{0}, nums2 = new int[]{1};
+        int m = 0, n = 1;
+        var expected = new int[]{1};
+        var result = MergeSortedArray.merge(nums1, m, nums2, n);
+        assertEquals(Arrays.toString(expected), Arrays.toString(result));
+    }
+}
