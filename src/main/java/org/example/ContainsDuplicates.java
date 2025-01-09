1e5935ef9c6ed882d7366b2b380c8f4de50c1774
Conor Sheppard <conorsheppardwork@gmail.com>
2025-01-09 18:16:46 +0000

    added ContainsDuplicates and tests

---
+++ src/main/java/org/example/ContainsDuplicates.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+public class ContainsDuplicates {
+    public static boolean containsDuplicates(int[] nums) {
+        List<Integer> numsList = Arrays.stream(nums).boxed().toList();
+        HashSet<Integer> numsSet = new HashSet<>(numsList);
+        return numsSet.size() < nums.length;
+    }
+}
---
+++ src/test/java/org/example/ContainsDuplicatesTest.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ContainsDuplicatesTest {
+
+    @Test
+    public void testCase1() {
+        int[] nums = {1, 2, 3, 1};
+        assertTrue(ContainsDuplicates.containsDuplicates(nums));
+    }
+
+    @Test
+    public void testCase2() {
+        int[] nums = {1, 2, 3, 4};
+        assertFalse(ContainsDuplicates.containsDuplicates(nums));
+    }
+
+    @Test
+    public void testCase3() {
+        int[] nums = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
+        assertTrue(ContainsDuplicates.containsDuplicates(nums));
+    }
+}
