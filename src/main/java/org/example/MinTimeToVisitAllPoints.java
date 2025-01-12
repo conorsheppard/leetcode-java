4b7c8b7699b80a2256f1d99eeb2f163be787d299
Conor Sheppard <conorsheppardwork@gmail.com>
2025-01-12 00:20:56 +0000

    added MinTimeToVisitAllPoints and tests

---
+++ src/main/java/org/example/MinTimeToVisitAllPoints.java
@@ -0,0 +1,74 @@
+package org.example;
+
+import java.util.*;
+
+public class MinTimeToVisitAllPoints {
+    public static int minTimeToVisitAllPoints(int[][] points) {
+        int seconds = 0;
+        var curPoints = points[0];
+        for (int i = 1; i < points.length; i++) {
+            var dist = Math.max(Math.abs(curPoints[0] - points[i][0]), Math.abs(curPoints[1] - points[i][1]));
+            seconds = seconds + dist;
+            curPoints = points[i];
+        }
+        return seconds;
+    }
+
+    public static int minTimeToVisitAllPointsStack(int[][] points) {
+        int seconds = 0;
+        List<List<Integer>> pointsList = new ArrayList<>(Arrays.stream(points).map(arr -> Arrays.stream(arr).boxed().toList()).toList());
+        Collections.reverse(pointsList);
+        Stack<List<Integer>> pointsStack = new Stack<>();
+        pointsStack.addAll(pointsList);
+        List<Integer> curPoints = pointsStack.pop();
+
+        for (int i = 1; i < points.length; i++) {
+            var dist = Math.max(Math.abs(Math.subtractExact(curPoints.get(0), points[i][0])),
+                    Math.abs(Math.subtractExact(curPoints.get(1), points[i][1])));
+            seconds = seconds + dist;
+            curPoints = pointsStack.pop();
+        }
+
+        return seconds;
+    }
+
+    public static int minTimeToVisitAllPoints2(int[][] points) {
+        int seconds = 0;
+
+        int curX = points[0][0];
+        int curY = points[0][1];
+
+        for (int i = 1; i < points.length; i++) {
+            int targetX = points[i][0];
+            int targetY = points[i][1];
+
+            if (curX == targetX) {
+                seconds = seconds + Math.abs(targetY - curY);
+                curY = targetY;
+            } else if (curY == targetY) {
+                seconds = seconds + Math.abs(targetX - curX);
+                curX = targetX;
+            } else {
+                var xDist = targetX - curX;
+                var yDist = targetY - curY;
+                if (Math.abs(xDist) > Math.abs(yDist)) {
+                    curX = curX + yDist;
+                    curY = targetY;
+                    seconds = seconds + Math.abs(yDist);
+                    xDist = targetX - curX;
+                    seconds = seconds + Math.abs(xDist);
+                    curX = targetX;
+                } else {
+                    curY = curY + xDist;
+                    curX = targetX;
+                    seconds = seconds + Math.abs(xDist);
+                    yDist = targetY - curY;
+                    seconds = seconds + Math.abs(yDist);
+                    curY = targetY;
+                }
+            }
+        }
+
+        return seconds;
+    }
+}
