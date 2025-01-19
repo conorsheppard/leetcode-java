382f2c6f7c5d634bcc8cc11997420664d577da07
Conor Sheppard <conorsheppardwork@gmail.com>
2025-01-19 11:20:28 +0000

    Spiral order solution using only streams

--- src/main/java/org/example/SpiralOrder.java
+++ src/main/java/org/example/SpiralOrder.java
@@ -8,7 +8,7 @@ import java.util.stream.IntStream;
 
 public class SpiralOrder {
 
-    public static List<Integer> spiralOrder(int[][] matrix) {
+    public static List<Integer> spiralOrder1(int[][] matrix) {
         if (matrix.length == 1) return Arrays.stream(matrix[0]).boxed().toList();
         if (Arrays.stream(matrix).noneMatch(arr -> arr.length > 1))
             return Arrays.stream(matrix).flatMapToInt(Arrays::stream).boxed().toList();
@@ -19,7 +19,7 @@ public class SpiralOrder {
         for (int i = 0; i <= Math.min(m, n) * 2 - 1 || res.size() == m * n; i++) {
             res.addAll(Arrays.stream(matrix[i], i, m - i).boxed().toList());
             if (res.size() == m * n) break;
-            int indexOffset = i;
+            final int indexOffset = i;
             res.addAll(Arrays.stream(matrix, i + 1, n - i).mapToInt(arr -> arr[arr.length - 1 - indexOffset]).boxed().toList());
             if (res.size() == m * n) break;
             List<Integer> rev = Arrays.stream(matrix[matrix.length - 1 - i]).boxed().sorted(Collections.reverseOrder((_, _) -> -1)).toList();
@@ -32,7 +32,82 @@ public class SpiralOrder {
         return res;
     }
 
-    public static List<Integer> spiralOrder1(int[][] matrix) {
+    public static List<Integer> spiralOrder(int[][] matrix) {
+        if (matrix.length == 1) return Arrays.stream(matrix[0]).boxed().toList();
+        if (Arrays.stream(matrix).noneMatch(arr -> arr.length > 1))
+            return Arrays.stream(matrix).flatMapToInt(Arrays::stream).boxed().toList();
+
+        List<Integer> res = new ArrayList<>();
+        int m = matrix[0].length;
+        int n = matrix.length;
+
+        IntStream.range(0, Math.min(m, n) * 2 - 1).anyMatch(i -> populateResult(res, matrix, i, m, n));
+
+        return res;
+    }
+
+    private static boolean populateResult(List<Integer> res, int[][] matrix, int i, int m, int n) {
+        if (addAllLeftToRight(res, matrix, i, m) == m * n) return true;
+        if (addAllTopToBottom(res, matrix, i, n) == m * n) return true;
+        if (addRightToLeft(res, matrix, i, m) == m * n) return true;
+        return addAllBottomToTop(res, matrix, i, n) == m * n;
+    }
+
+    private static int addAllLeftToRight(List<Integer> res, int[][] matrix, int i, int m) {
+        res.addAll(Arrays.stream(matrix[i], i, m - i).boxed().toList());
+        return res.size();
+    }
+
+    private static int addAllTopToBottom(List<Integer> res, int[][] matrix, int i, int n) {
+        res.addAll(Arrays.stream(matrix, i + 1, n - i).mapToInt(arr -> arr[arr.length - 1 - i]).boxed().toList());
+        return res.size();
+    }
+
+    private static int addRightToLeft(List<Integer> res, int[][] matrix, int i, int m) {
+        List<Integer> rev = Arrays.stream(matrix[matrix.length - 1 - i]).boxed().sorted(Collections.reverseOrder((_, _) -> -1)).toList();
+        res.addAll(IntStream.range(i + 1, m - i).map(rev::get).boxed().toList());
+        return res.size();
+    }
+
+    private static int addAllBottomToTop(List<Integer> res, int[][] matrix, int i, int n) {
+        List<Integer> rev2 = Arrays.stream(matrix).sorted(Collections.reverseOrder((_, _) -> -1)).mapToInt(arr -> arr[i]).boxed().toList();
+        res.addAll(IntStream.range(i + 1, n - 1 - i).map(rev2::get).boxed().toList());
+        return res.size();
+    }
+
+    public static List<Integer> spiralOrderOneMethod(int[][] matrix) {
+        if (matrix.length == 1) return Arrays.stream(matrix[0]).boxed().toList();
+        if (Arrays.stream(matrix).noneMatch(arr -> arr.length > 1))
+            return Arrays.stream(matrix).flatMapToInt(Arrays::stream).boxed().toList();
+
+        List<Integer> res = new ArrayList<>();
+        int m = matrix[0].length;
+        int n = matrix.length;
+        for (int i = 0; i <= Math.min(m, n) * 2 - 1 || res.size() == m * n; i++) {
+            addAll(res, matrix, i, m);
+            if (res.size() == m * n) break;
+            int indexOffset = i;
+            Arrays.stream(matrix, i + 1, n - i).mapToInt(arr -> arr[arr.length - 1 - indexOffset]).toArray();
+            var intArray = Arrays.stream(matrix, i + 1, n - i).mapToInt(arr -> arr[arr.length - 1 - indexOffset]).toArray();
+            var newMatrix = new int[i + 1][n];
+            newMatrix[i] = intArray;
+            addAll(res, newMatrix, i, n - 1);
+            if (res.size() == m * n) break;
+            List<Integer> rev = Arrays.stream(matrix[matrix.length - 1 - i]).boxed().sorted(Collections.reverseOrder((_, _) -> -1)).toList();
+            res.addAll(IntStream.range(i + 1, m - i).map(rev::get).boxed().toList());
+            if (res.size() == m * n) break;
+            List<Integer> rev2 = Arrays.stream(matrix).sorted(Collections.reverseOrder((_, _) -> -1)).mapToInt(arr -> arr[indexOffset]).boxed().toList();
+            res.addAll(IntStream.range(i + 1, n - 1 - i).map(rev2::get).boxed().toList());
+        }
+
+        return res;
+    }
+
+    private static void addAll(List<Integer> list, int[][] matrix, int i, int dimension) {
+        list.addAll(Arrays.stream(matrix[i], i, dimension - i).boxed().toList());
+    }
+
+    public static List<Integer> spiralOrder2(int[][] matrix) {
         if (matrix.length == 1) return Arrays.stream(matrix[0]).boxed().toList();
         if (Arrays.stream(matrix).noneMatch(arr -> arr.length > 1))
             return Arrays.stream(matrix).flatMapToInt(Arrays::stream).boxed().toList();
