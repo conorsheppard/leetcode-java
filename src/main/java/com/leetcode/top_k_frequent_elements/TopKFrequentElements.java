b33dad6666a47bf3e74ffa76f12023dab96a9580
Conor Sheppard <conorsheppardwork@gmail.com>
2025-04-18 12:16:08 +0100

    added 347. Top K Frequent Elements

---
+++ src/main/java/com/leetcode/top_k_frequent_elements/TopKFrequentElements.java
@@ -0,0 +1,52 @@
+package com.leetcode.top_k_frequent_elements;
+
+import java.util.*;
+import java.util.Map.Entry;
+
+public class TopKFrequentElements {
+    static int[] topKFrequent(int[] nums, int k) {
+        if (nums.length == 0 || k == 0) return new int[]{};
+
+        var map = new HashMap<Integer, Integer>();
+        var heap = new PriorityQueue<>(Comparator.comparing(Node::freq).reversed());
+        var res = new int[k];
+
+        for (int num : nums) map.put(num, map.getOrDefault(num, 0) + 1);
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) heap.add(new Node(entry.getKey(), entry.getValue()));
+        for (int i = 0; i < k; i++) res[i] = heap.poll().num;
+
+        return res;
+    }
+
+    // private indicates intent of use, this is just a utility Record to be used solely in this class
+    private record Node(Integer num, Integer freq) { // implements Comparable<Node> {
+//        @Override
+//        public int compareTo(Node node) {
+//            return Integer.compare(node.freq(), freq);
+//        }
+    }
+
+    static int[] topKFreq(int[] nums, int k) {
+        if (nums.length == 0 || k == 0) return new int[]{};
+        var res = new int[k];
+        var m = new HashMap<Integer, Integer>();
+        var pq = new PriorityQueue<>(Comparator.comparing(Entry<Integer, Integer>::getValue));
+        for (int n : nums) m.put(n, m.getOrDefault(n, 0) + 1);
+        pq.addAll(m.entrySet());
+        for (int i = 0; i < k; i++) res[i] = pq.poll().getValue();
+        return res;
+    }
+
+    static int[] topKFreqFunctional(int[] nums, int k) {
+        if (nums.length == 0 || k == 0) return new int[0];
+
+        var freqMap = new HashMap<Integer, Integer>();
+        for (int num : nums) freqMap.merge(num, 1, Integer::sum);
+
+        return freqMap.entrySet().stream()
+                .sorted((a, b) -> b.getValue() - a.getValue())
+                .limit(k)
+                .mapToInt(Map.Entry::getKey)
+                .toArray();
+    }
+}
---
+++ src/main/java/com/leetcode/top_k_frequent_elements/TopKFrequentElementsTest.java
@@ -0,0 +1,37 @@
+package com.leetcode.top_k_frequent_elements;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static com.leetcode.top_k_frequent_elements.TopKFrequentElements.topKFrequent;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TopKFrequentElementsTest {
+    static Stream<Arguments> provideTestCases() {
+        return Stream.of(
+                Arguments.of(new int[]{1, 1, 1, 2, 2, 3}, 2, List.of(1, 2)),
+                Arguments.of(new int[]{1}, 1, List.of(1)),
+                Arguments.of(new int[]{2, 2, 3, 1, 1, 1}, 2, List.of(1, 2)),
+                Arguments.of(new int[]{}, 2, List.of()),
+                Arguments.of(new int[]{2, 2, 3, 3, 3, 3, 3, 1, 1, 1}, 2, List.of(1, 3))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideTestCases")
+    void testTopKFrequent(int[] nums, int k, List<Integer> expected) {
+        var result = topKFrequent(nums, k);
+        assertTrue(arrayConv(result).containsAll(expected) && expected.containsAll(arrayConv(result)),
+                "Expected " + expected + " but got " + arrayConv(result));
+    }
+
+    private static List<Integer> arrayConv(int[] result) {
+        return Arrays.stream(result).boxed().toList();
+    }
+}
