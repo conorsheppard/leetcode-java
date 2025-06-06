8963cfff6d45b37097c6c1026433b50e3fe346d7
Conor Sheppard <conorsheppardwork@gmail.com>
2025-06-06 18:43:41 +0100

    improved readability

--- src/main/java/com/leetcode/simple_bank_system_2043/Bank.java
+++ src/main/java/com/leetcode/simple_bank_system_2043/Bank.java
@@ -11,26 +11,31 @@ class Bank {
 
     public boolean transfer(int account1, int account2, long money) {
         if (!allValidAccounts(account1, account2)) return false;
-        // todo: wrap these in a transaction
         if (!hasSufficientFunds(account1, money)) return false;
-        accounts[account1 - 1] = accounts[account1 - 1] - money;
-        // todo: make sure the addition doesn't cause integer overflow
-        accounts[account2 - 1] = accounts[account2 - 1] + money;
+        debitAccount(account1, money);
+        creditAccount(account2, money);
         return true;
     }
 
     public boolean deposit(int account, long money) {
         if (!isValidAccount(account)) return false;
-        // todo: make sure the addition doesn't cause integer overflow
-        accounts[account - 1] = accounts[account - 1] + money;
+        creditAccount(account, money);
         return true;
     }
 
     public boolean withdraw(int account, long money) {
         if (!isValidAccount(account)) return false;
         if (!hasSufficientFunds(account, money)) return false;
-        accounts[account - 1] = accounts[account - 1] - money;
+        debitAccount(account, money);
         return true;
+    }
+
+    private void creditAccount(int account, long amount) {
+        accounts[account - 1] = accounts[account - 1] + amount;
+    }
+
+    private void debitAccount(int account, long amount) {
+        accounts[account - 1] = accounts[account - 1] - amount;
     }
 
     private boolean hasSufficientFunds(int account, long money) {
