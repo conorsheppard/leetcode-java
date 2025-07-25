314e7d2de045149a856adfa816b162a85e734e18
Conor Sheppard <conorsheppardwork@gmail.com>
2025-07-25 23:11:39 +0100

    feat(CombinationsTest.java): working for all test cases

--- src/main/java/com/leetcode/combinations_77/Combinations.java
+++ src/main/java/com/leetcode/combinations_77/Combinations.java
@@ -2,11 +2,13 @@ package com.leetcode.combinations_77;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Combinations {
     public List<List<Integer>> combine(int n, int k) {
-        var res = new ArrayList<List<Integer>>(); // res.get(i).size() == k, i.e. inner List<Integer> size == k
-        for (int i = 1; i < n; i++) {
+        if (n == 1) return List.of(IntStream.rangeClosed(1, k).boxed().toList());
+        var res = new ArrayList<List<Integer>>();
+        for (int i = 1; i <= n; i++) {
             combineDfs(k, n, new ArrayList<>(), res, 1, i);
         }
 
@@ -25,19 +27,5 @@ public class Combinations {
             combineDfs(maxDepth, maxBranches, new ArrayList<>(curComb), combinations, curDepth + 1, curVal + 1);
             curVal++;
         }
-    }
-
-    public List<List<Integer>> combineIterative(int n, int k) {
-        var res = new ArrayList<List<Integer>>(); // res.get(i).size() == k, i.e. inner List<Integer> size == k
-        for (int i = 1; i <= n; i++) {
-            for (int j = i; j <= n; j++) {
-                var combination = new ArrayList<Integer>();
-                for (int q = 1; q <= k; q++) {
-                    combination.add(j);
-                }
-                res.add(combination);
-            }
-        }
-        return res;
     }
 }
